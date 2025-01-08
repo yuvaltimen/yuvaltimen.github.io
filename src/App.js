6b3b3e13c879a926042a93373ba825c372a1d51a
Yuval Timen <yuvaltimen@gmail.com>
2025-01-08 10:39:29 -0500

    Migrate to React wip

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-// import logo from './logo.svg';
 import './App.css';
 import Layout from './pages/Layout';
 import NotFound from './pages/NotFound';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route exact path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
